94942332eaa0aba3d469037b497bf37fb776e38b
forkcpu <forkxit@gmail.com>
2024-11-20 14:16:29 +0530

    Create Round-Robin-set2.cpp

---
+++ Round-Robin-set2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum) {
+    int rem_bt[n];
+    for (int i = 0; i < n; i++) {
+        rem_bt[i] = bt[i];
+    }
+
+    int t = 0;
+    while (true) {
+        bool done = true;
+
+        for (int i = 0; i < n; i++) {
+            if (rem_bt[i] > 0) {
+                done = false;
+                if (rem_bt[i] > quantum) {
+                    t += quantum;
+                    rem_bt[i] -= quantum;
+                } else {
+                    t += rem_bt[i];
+                    wt[i] = t - bt[i];
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+
+        if (done) break;
+    }
+}
+
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findAvgTime(int processes[], int n, int bt[], int quantum) {
+    int wt[n], tat[n];
+    findWaitingTime(processes, n, bt, wt, quantum);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    cout << "Processes\tBurst Time\tWaiting Time\tTurnaround Time\n";
+    for (int i = 0; i < n; i++) {
+        cout << processes[i] << "\t\t" << bt[i] << "\t\t" << wt[i] << "\t\t" << tat[i] << endl;
+    }
+
+    float total_wt = 0, total_tat = 0;
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+    cout << "Average waiting time = " << total_wt / n << endl;
+    cout << "Average turnaround time = " << total_tat / n << endl;
+}
+
+int main() {
+    int processes[] = {1, 2, 3};
+    int n = sizeof(processes) / sizeof(processes[0]);
+    int burst_time[] = {10, 5, 8};
+    int quantum = 2;
+
+    findAvgTime(processes, n, burst_time, quantum);
+    return 0;
+}
